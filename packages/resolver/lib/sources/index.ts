a472414bd79f04d010d06858a4c00913c3651f63
g. nicholas d'andrea <gnidan@trufflesuite.com>
2020-09-24 03:17:17 -0400

    Allow importing ABI JSON files in Solidity
    Define ABI resolver source that extends from FS resolver source:
    
    - Support only `resolve()` (explicitly exclude `require()`)
    - Catch imports that end in `.json` or `.abi.json`
    - Defer to `abi-to-sol` to generate Solidity source

--- packages/resolver/lib/sources/index.ts
+++ packages/resolver/lib/sources/index.ts
@@ -2,6 +2,7 @@ import { EthPMv1 } from "./ethpm-v1";
 import { NPM } from "./npm";
 import { GlobalNPM } from "./globalnpm";
 import { FS } from "./fs";
+import { ABI } from "./abi";
 import { ResolverSource } from "../source";
 
 export function constructSources(options: any): ResolverSource[] {
@@ -9,6 +10,7 @@ export function constructSources(options: any): ResolverSource[] {
     new EthPMv1(options.working_directory),
     new NPM(options.working_directory),
     new GlobalNPM(),
+    new ABI(options.working_directory, options.contracts_build_directory),
     new FS(options.working_directory, options.contracts_build_directory)
   ];
 }
